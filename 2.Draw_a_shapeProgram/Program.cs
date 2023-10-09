f01e3a05a0b56972817b62afa75127ac4ffac17a
Lenovo-User <Lenovo-User@DESKTOP-RNPFAS5>
2023-10-09 10:50:03 +0200

    Add project files.

---
+++ 2.Draw_a_shapeProgram/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2.Draw_a_shapeProgram
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("      /|");
+            Console.WriteLine("     / |");
+            Console.WriteLine("    /  |");
+            Console.WriteLine("   /   |");
+            Console.WriteLine("  /    |");
+            Console.WriteLine(" /     |");
+            Console.WriteLine(" ------ ");
+
+            // freeze console
+            Console.ReadLine();
+        }
+    }
+}
